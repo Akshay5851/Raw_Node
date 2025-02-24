6d3034690fa14cd13e202002aa41de340ef5624f
akkisaini@bitbucket.com <akshay.saini@idslogic.com>
2025-02-24 16:53:48 +0530

    24-2-2025

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-const express = require('express')
-const connection= require('./connection')
+const express = require('express');
+const connection= require('./connection');
 //const con = require('./users/view')
-const app = express()
-const port = 4000
+const jwt = require('jsonwebtoken');
+const bodyparser = require('body-parser');
+const becrypt = require('bcryptjs');
+const app = express();
+const port= process.env.PORT;
+app.use(bodyparser.json());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+// Home page route
 
 app.get('/',(req,res)=>{
     res.send('first page working finess')
 })
 
+// Register a new user
+
+app.post('/register',(req,res)=>{
+  try{
+    const {username,email}= req.body;
+    const cleanedEmail = email.trim().toLowerCase();
+    
+    //console.log(cleanedEmail);
+    connection.query('select * from users where email = ?',[email],(err,result)=>{
+
+      if (err) {
+      return res.status(500).json({ error: "Database error during email check", details: err });
+      }
+      //console.log(result);
+      if (result.length > 0) {
+      return res.status(400).json({ message: "Email already registered" });
+      }
+      
+    })
+    
+      connection.query(`insert into users (username,email) values( ?,?)`,[username,email],(err,result)=>{
+        if(err){
+          return res.status(500).json({ error: "Database error during user insertion", details: err });
+          //console.log('please fix this error err',err)
+        }else{
+          console.log(result)
+          return res.status(201).json({ message: "User successfully registered", userId: result.insertId });
+        }
+      })
+    
+
+  }
+    catch(error){
+
+      res.status(500).json({ error: "Unexpected server error", details: error.message });
+    }
+
+})
 
 app.get('/users',(req,res)=>{
     const con=connection.query('select * from users',(err,result)=>{
@@ -17,7 +63,7 @@ app.get('/users',(req,res)=>{
         }
         console.log('query is succesful')
          res.json(result);
-        console.log(result)
+        //console.log(result)
       })
 })
 
